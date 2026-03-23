5e486d36fb49d81210ac2270a96709eecf6f542f
Big-Stan <244668394+stanleyokyemanarthur-max@users.noreply.github.com>
2026-03-23 10:34:58 +0000

    Update sendReceipt.js

--- Backend/utils/sendReceipt.js
+++ Backend/utils/sendReceipt.js
@@ -22,12 +22,17 @@ export const sendReceipt = async ({
        TRANSPORTER
     ========================= */
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 10000, // 🔥 prevent hanging
+  greetingTimeout: 10000,
+  socketTimeout: 10000
+});
 
     /* =========================
        GENERATE RECEIPT PDF
